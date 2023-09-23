b99838a756978766f27ea450221db508c1b15ed9
Rahul-CSEstudent <talktorahul.b@gmail.com>
2023-09-23 12:21:12 +0530

    commit

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export const Navbar: FC = () => {
     return (
-        <div className="bg-blue-500 flex justify-evenly p-4">
+        <div className="bg-blue-500 flex justify-between p-4">
             <div className='flex items-center justify-center'><Image alt="img" src="/logo.png" height={20} width={40} /> <span className='ml-2 text-xl font-bold'>E vault</span></div>
             {/* <span className="text-white">Blockchain based E-Vault</span> */}
             <WalletMultiButton />
